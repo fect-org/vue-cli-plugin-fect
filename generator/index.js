261573978213fffc7afbd53a7314ceba7a7dad04
kanno <812137533@qq.com>
2022-01-16 17:26:32 +0800

    feat: use latest version

--- generator/index.js
+++ generator/index.js
@@ -1,35 +1,44 @@
-module.exports = (api, opts, rootOptions) => {
+const latestVersion = require("latest-version");
+
+const FECT_VUE = "@fect-ui/vue";
+
+const BABEL_PLUGIN_IMPORT = "babel-plugin-import";
+
+const resolvePkgVersion = async (pkg) => {
+  const lastest = await latestVersion(pkg);
+  return {
+    [pkg]: lastest,
+  };
+};
+
+module.exports = async (api, opts, rootOptions) => {
   api.extendPackage({
-    dependencies: {
-      '@fect-ui/vue': '^1.2.0-rc.0',
-    },
+    dependencies: await resolvePkgVersion(FECT_VUE),
   });
 
   // Babel 按需引入
-  if (opts.importType === 'partial' && opts.partialImportType === 'babel') {
+  if (opts.importType === "partial" && opts.partialImportType === "babel") {
     api.extendPackage({
-      devDependencies: {
-        'babel-plugin-import': '^1.13.3',
-      },
+      devDependencies: await resolvePkgVersion(BABEL_PLUGIN_IMPORT),
     });
 
     api.render({
-      './babel.config.js': './templates/babel.config.js',
-      './src/App.vue': './templates/App.vue',
+      "./babel.config.js": "./templates/babel.config.js",
+      "./src/App.vue": "./templates/App.vue",
     });
   } else {
     api.injectImports(api.entryFile, `import createFect from './plugins/fect'`);
 
     api.render({
-      './src/plugins/fect.js': './templates/plugins/fect.js',
-      './src/App.vue': './templates/App.vue',
+      "./src/plugins/fect.js": "./templates/plugins/fect.js",
+      "./src/App.vue": "./templates/App.vue",
     });
 
     api.afterInvoke(() => {
-      const { EOL } = require('os');
-      const fs = require('fs');
+      const { EOL } = require("os");
+      const fs = require("fs");
       const contentMain = fs.readFileSync(api.resolve(api.entryFile), {
-        encoding: 'utf-8',
+        encoding: "utf-8",
       });
       const lines = contentMain.split(/\r?\n/g);
 
@@ -39,10 +48,10 @@ module.exports = (api, opts, rootOptions) => {
       const renderContent = lines[renderIndex];
       lines[renderIndex] = `const app = createApp(App)`;
       lines[renderIndex + 1] = `createFect(app)`;
-      lines[renderIndex + 2] = renderContent.replace('createApp(App)', 'app');
+      lines[renderIndex + 2] = renderContent.replace("createApp(App)", "app");
 
       fs.writeFileSync(api.resolve(api.entryFile), lines.join(EOL), {
-        encoding: 'utf-8',
+        encoding: "utf-8",
       });
     });
   }
